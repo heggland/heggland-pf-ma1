799a6a54dc5b37436e2223fa0ad0a8de803d2b37
heggland <kjetilheggland@gmail.com>
2020-05-20 20:40:35 +0200

    upload

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+
+// 1. Declare a variable called firstVar but don't initialise it with a value.
+
+var firstVar;
+
+// 2. Declare a variable called name and assign it your first name.
+
+var name = 'Kjetil';
+console.log(name);
+
+// 3. Declare and initialise a variable with a number value.
+
+var number = 12;
+console.log(number);
+
+// 4. Create a variable called division and initialise it with a value of 20 divided by 5.
+
+var division = 20 / 5;
+console.log(division);
+
+// 5. Write code that checks the type of the value "frog".
+
+var animal = 'frog';
+console.log(typeof animal);
+
+// 6. Declare and initialise a variable called orderHasShipped with a boolean value.
+
+var orderHasShipped = true;
+
+// 7. Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+
+if(orderHasShipped === true) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+
+
+// 8. Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
+
+/*
+Create a repository in your GitHub account called {your-name}-pf-ma1, e.g. mary-smith-pf-ma1, and make sure it's public
+Create a file called script.js
+Write your answers inside this file with a comment above each answer indicating the question number:
+ */
